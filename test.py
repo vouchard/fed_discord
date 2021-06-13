26c014f3e90621636935c80e92ee03a41ce4785e
vouchard <38253082+vouchard@users.noreply.github.com>
2021-06-13 10:32:11 +0800

    adding boto3 and specifying aws credentials

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 from fed_module import *
 import datetime
 import os
+import boto3
+from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key
 
+
+
+aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+
+s3 = boto3.resource('s3',
+         aws_access_key_id=aws_access_key_id,
+         aws_secret_access_key= aws_secret_access_key)
 
 rp = dynamo_comms()
 print(rp.add_response_on_word('1234', 'test', 'test_responsse2', 'vou'))
