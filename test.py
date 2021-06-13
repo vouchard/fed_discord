feb3aed34a6442ba2a6aecfe3b80669e50b46376
vouchard <38253082+vouchard@users.noreply.github.com>
2021-06-13 10:17:47 +0800

    removing discord module on test

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-import discord #discord.py
 from fed_module import *
 import datetime
 import os
-from discord.ext import commands
+
 
 rp = dynamo_comms()
 print(rp.add_response_on_word('1234', 'test', 'test_responsse2', 'vou'))
