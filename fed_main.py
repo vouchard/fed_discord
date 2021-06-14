97497a531b5602db896d766a1044c7d5ab8f3f4e
vouchard <38253082+vouchard@users.noreply.github.com>
2021-06-14 16:32:57 +0800

    Update fed_main.py

--- fed_main.py
+++ fed_main.py
@@ -13,33 +13,4 @@ client.remove_command('help')
 
 database = dynamo_comms()
 rp = dynamo_comms()
-@client.event
-async def on_ready():
-    print ("bot is ready")
-
-#--------------------RESPONSES-----------------------------------
-@client.command()
-async def addResponse(ctx,qfiltered_word,response):
-    server = str(ctx.message.guild.id)
-    filtered_word = qfiltered_word.upper()
-    author = ctx.message.author.name
-    rp.add_response_on_word(server, qfiltered_word, response, author)
-    await ctx.send('Response added')
-
-@client.command()
-async def viewResponse(ctx,qword):
-    server = ctx.message.guild.id
-    await ctx.send(rp.viewResponse(qword,server))
-
-@client.command()
-async def removeResponse(ctx,rid):
-    rp.removeResponse(rid)
-    await ctx.send('Response removed')
-
-
-
-
-
-print('waiting for client.run. . . ')
-#client.run(discord_token)
 
