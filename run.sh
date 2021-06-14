8abd077a132d732e70ded2f6446a906a27ba6415
vouchard <38253082+vouchard@users.noreply.github.com>
2021-06-14 17:58:06 +0800

    passing parameter to py script

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+cd /ec2-user/credentials
+DISCORD_KEY=$(cat DISCORD_KEY.env)
 cd /home/ec2-user/fed_discord
 source environment/bin/activate
 sudo chmod +x fed_main.py
-./fed_main.py
+./fed_main.py $DISCORD_KEY
 
