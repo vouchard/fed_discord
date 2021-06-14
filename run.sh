cb2428f6c2ef73f25889beeb602945ab4337da8d
vouchard <38253082+vouchard@users.noreply.github.com>
2021-06-14 21:12:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ DISCORD_KEY=$(cat DISCORD_KEY.env)
 cd /home/ec2-user/fed_discord
 source environment/bin/activate
 sudo chmod +x fed_main.py
-nohup ./fed_main.py $DISCORD_KEY &
+nohup ./fed_main.py $DISCORD_KEY > /dev/null 2> /dev/null < /dev/null &
 
