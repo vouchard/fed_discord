c1a540f6d543bbbc9eb83f81926c6e608f92c719
vouchard <38253082+vouchard@users.noreply.github.com>
2021-06-14 16:35:50 +0800

    Update fed_main.py

--- fed_main.py
+++ fed_main.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
-
 import discord #discord.py
 from fed_module import *
 import datetime
 import os
 from discord.ext import commands
-###DISCORD
-discord_token = os.environ['DISCORD_KEY']
-
-client = commands.Bot(command_prefix = 'fd.')
-client.remove_command('help')
-
-database = dynamo_comms()
-rp = dynamo_comms()
-
