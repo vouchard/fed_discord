1ece9589d7c335418cadd74a1bfba19555854e08
vouchard <38253082+vouchard@users.noreply.github.com>
2021-06-15 19:51:34 +0800

    initialize screen

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-cd /home/ec2-user/credentials
-DISCORD_KEY=$(cat DISCORD_KEY.env)
 cd /home/ec2-user/fed_discord
 source environment/bin/activate
 sudo chmod +x fed_main.py
-./fed_main.py $DISCORD_KEY > /dev/null 2> /dev/null < /dev/null &
+screen -d -m -S fed_bot  ./fed_main.py
+
+
+
 
